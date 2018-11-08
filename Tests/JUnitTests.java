7a066f85fa94c2825e472cf1aaf1f33719caef32
Dov Sturm <dysturm@gmail.com>
2018-11-07 20:18:18 -0500

    Initial commit

---
+++ Tests/JUnitTests.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class JUnitTests {
+
+    @Test
+    void SMTPCommunicatorTest(){
+
+
+        //assertEquals(9, result);
+    }
+    @Test
+    void DocumentHandlerTest(){
+
+        //assertEquals(2, result);
+    }
+}
